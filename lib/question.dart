0a1423c32653cea14d39411662d002571c341a4a
Carlos Gonzalez <cagh.heviag@gmail.com>
2020-04-26 16:58:58 -0400

    Commit 1

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String _questionText;
+
+  String get questionText => _questionText;
+
+  bool _questionAnswers;
+
+  bool get questionAnswers => _questionAnswers;
+
+  Question(this._questionText, this._questionAnswers);
+}
